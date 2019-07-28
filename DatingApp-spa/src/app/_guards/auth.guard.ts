0f533044cd815a2cab9c49fd938ec20e09003812
Seth <seth@360labs.io>
2019-07-28 11:32:51 -0500

    End of Module 7.

---
+++ DatingApp-spa/src/app/_guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AlertifyService } from '../services/alertify.service';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+	constructor(private authService:AuthService, private router: Router, private alertify: AlertifyService) {}
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean  {
+		if(this.authService.loggedIn()) {
+			return true;
+		}
+		this.alertify.error('You shall not pass!!!');
+		this.router.navigate(['/home']);
+		return false;
+	}
+
+}
